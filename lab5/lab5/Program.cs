81a9bb75b96b29bdc22e0b52dd7450bd2fc3bffc
Paweena27 <59030185@kmitl.ac.th>
2017-09-08 11:36:51 +0700

    W05P01-6

--- lab5/lab5/Program.cs
+++ lab5/lab5/Program.cs
@@ -5,7 +5,18 @@ namespace lab5
     {
         static void Main(string[] args)
         {
+            object o2 = null;
+            try
+            {
+                int i2 = (int)o2;
+                Console.WriteLine("i2 = {0}", i2);
+            }
+            catch
+            {
+                Console.WriteLine("Error, null object assignment.");
 
+            }
         }
+
     }
 }
