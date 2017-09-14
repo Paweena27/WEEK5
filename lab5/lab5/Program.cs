b55ab15b07333f554355beed1c44b1f266c0e7ce
Paweena27 <59030185@kmitl.ac.th>
2017-09-14 14:52:11 +0700

    W05P01-7

--- lab5/lab5/Program.cs
+++ lab5/lab5/Program.cs
@@ -5,6 +5,20 @@ namespace lab5
     {
         static void Main(string[] args)
         {
+            int a = 0;
+            try
+            {
+
+                Console.WriteLine(100 / a);
+            }
+            catch (NullReferenceException e)
+            {
+                Console.WriteLine(e.Message);
+            }
+            catch (DivideByZeroException e)
+            {
+                Console.WriteLine(e.Message);
+            }
 
         }
     }
