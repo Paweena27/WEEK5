511258ce02b5bb0605f59ab2a1132cfc58b0fbe1
Paweena27 <59030185@kmitl.ac.th>
2017-09-14 20:12:58 +0700

    W05P01-19

--- lab5/lab5/Program.cs
+++ lab5/lab5/Program.cs
@@ -6,6 +6,19 @@ namespace lab5
         static void Main(string[] args)
         {
 
+            int point = 68;
+            if (point < 50)
+                Console.WriteLine("Grade F");
+            else if (point < 60)
+                Console.WriteLine("Grade D");
+            else if (point < 70)
+                Console.WriteLine("Grade C");
+            else if (point < 80)
+                Console.WriteLine("Grade B");
+            else
+                Console.WriteLine("Grade A");
         }
+
     }
 }
+
