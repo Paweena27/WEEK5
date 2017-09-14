73e9a9fbe2348cc64c054f9d95bde4d2d389e14f
Paweena27 <59030185@kmitl.ac.th>
2017-09-14 15:18:22 +0700

    W05P01-11

--- lab5/lab5/Program.cs
+++ lab5/lab5/Program.cs
@@ -5,6 +5,13 @@ namespace lab5
     {
         static void Main(string[] args)
         {
+            int a = 10;
+            int b = 20;
+            int c = add(a, b);
+        }
+        private static int add(int a, int b)
+        {
+            throw new NotImplementedException();
 
         }
     }
