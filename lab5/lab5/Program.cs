9ba93219278710b0435f9dff6e3c20da74ef3f07
Paweena27 <59030185@kmitl.ac.th>
2017-09-14 20:39:41 +0700

    W05P01-27

--- lab5/lab5/Program.cs
+++ lab5/lab5/Program.cs
@@ -5,6 +5,18 @@ namespace lab5
     {
         static void Main(string[] args)
         {
+            for (int i = 1; i <= 100; i++)
+            {
+                if (i == 5)
+                {
+                    break;
+                }
+                Console.WriteLine(i);
+            }
+
+            // Keep the console open in debug mode.
+            Console.WriteLine("Press any key to exit.");
+            Console.ReadKey();
 
         }
     }
