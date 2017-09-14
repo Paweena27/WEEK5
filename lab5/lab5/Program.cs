58ac1c5a095a19ca4ea387ca38f207820a9fb31a
Paweena27 <59030185@kmitl.ac.th>
2017-09-14 15:42:46 +0700

    W05P01-13

--- lab5/lab5/Program.cs
+++ lab5/lab5/Program.cs
@@ -5,7 +5,35 @@ namespace lab5
     {
         static void Main(string[] args)
         {
+            int a = 10;
+            int b = 20;
+            int c;
+            try
+            {
+                c = div(a, b);
+            }
+            catch (DivideByZeroException e)
+            {
 
+                Console.WriteLine("DivideByZeroException");
+                Console.WriteLine(e.Message);
+            }
+            catch (NullReferenceException e)
+            {
+                Console.WriteLine("NullReferenceException");
+                Console.WriteLine(e.Message);
+
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("Exception");
+                Console.WriteLine(e.Message);
+            }
+        }
+        private static int div(int a, int b)
+        {
+            throw new NullReferenceException();
         }
     }
-}
+    }
+
