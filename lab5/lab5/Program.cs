12292ec194b81b3e5c5e12db71684bf7c24009a3
Paweena27 <59030185@kmitl.ac.th>
2017-09-14 20:35:51 +0700

    W05P01-25

--- lab5/lab5/Program.cs
+++ lab5/lab5/Program.cs
@@ -5,6 +5,10 @@ namespace lab5
     {
         static void Main(string[] args)
         {
+            for (int i = 1; i <= 12; i++)
+            {
+                Console.WriteLine("{0, 2} x 2 = {1, 2}", i, i * 2);
+            }
 
         }
     }
